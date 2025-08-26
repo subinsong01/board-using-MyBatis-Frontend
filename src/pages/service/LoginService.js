0db5966cb96035d1f39b0717b9e2c55a055929ab
subinsong <haely97@naver.com>
2025-08-27 08:29:33 +0900

    feat: 로그인 서비스 api 경로 오류 수정

--- src/pages/service/LoginService.js
+++ src/pages/service/LoginService.js
@@ -1,10 +1,10 @@
-import { login, getMyInfo, logout } from "@/features/login/LoginApi";
+import { authApi } from "@/features/login/LoginApi";
 
 export const LoginService = {
   //로그인 시도
   async login({ username, password }) {
     try {
-      const data = await login({ username, password });
+      const data = await authApi.login({ username, password });
       return data;
     } catch (error) {
       console.error("로그인 시도 에러", error);
@@ -14,7 +14,7 @@ export const LoginService = {
   //정보 가져오기
   async getMyInfo() {
     try {
-      const data = await getMyInfo();
+      const data = await authApi.getMyInfo();
       return data;
     } catch (error) {
       console.error("유저 정보 가지고 오기 에러", error);
@@ -25,7 +25,7 @@ export const LoginService = {
   //로그아웃
   async logout() {
     try {
-      await logout(); //세션 종료 요청
+      await authApi.logout(); //세션 종료 요청
     } catch (error) {
       console.log("로그아웃 실패 ~~~~~", error);
       throw error;
